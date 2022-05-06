493bcc9e9fa09cbf438089535f6432c17f3eb608
Rod Doe <doerodney@gmail.com>
2022-05-05 20:41:16 -0700

    Added start of add-two-numbers solution.

---
+++ add-two-numbers/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+)
+
+type ListNode struct {
+  Val int
+  Next *ListNode
+}
+
+type LinkedList struct {
+	Head *ListNode
+	Len int
+}
+
+
+func (l * LinkedList) insert(val int) {
+	node := &ListNode{Val: val, Next: nil}
+	if l.Len == 0 {
+		l.Head = node
+		l.Len++
+	} else {
+		ptr := l.Head
+		for i := 0; i < l.Len; i++ {
+			if ptr.Next == nil {
+				ptr.Next = node
+				l.Len++
+				break
+			} else {
+				ptr = ptr.Next
+			}
+		}
+	}
+}
+
+
+func getNumber(list *ListNode) int {
+	multiplier := 1
+	number := 0
+	ptr := list
+	for ptr != nil {
+		number += ptr.Val * multiplier
+		ptr = ptr.Next
+		multiplier *= 10
+	}
+	return number
+}
+
+
+func main() {
+	xl := LinkedList{}
+	yl := LinkedList{}
+
+	xd := []int{2, 4, 3,}
+	yd := []int{5, 6, 4,}
+
+	for _, x := range(xd) {
+		xl.insert(x)
+	}
+
+	xn := getNumber(xl.Head)
+
+	for _, y := range(yd) {
+		yl.insert(y)
+	}
+	yn := getNumber(yl.Head)
+
+
+	fmt.Printf("Slice: %v, Number: %d\n", xd, xn)
+	fmt.Printf("Slice: %v, Number: %d\n", yd, yn)
+}
